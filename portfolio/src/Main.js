69d398cc4bcff8c5d4c8264863ca9ae1e8f4b4a1
Mark Turner <mark.edward.turner@gmail.com>
2023-02-25 14:20:46 -0500

    Added Main.js as landing page structure

---
+++ portfolio/src/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+ 
+class Main extends Component {
+  render() {
+    return (
+        <div>
+          <h1>Mark Edward Turner Portfolio</h1>
+          <ul className="header">
+            <li><a href="/">Home</a></li>
+            <li><a href="/stuff">Projects</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+          <div className="content">
+             
+          </div>
+        </div>
+    );
+  }
+}
+ 
+export default Main;
