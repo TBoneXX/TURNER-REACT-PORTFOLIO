b10033c14b31fafb853dcfd90f7f540f89dd95cf
Mark Turner <mark.edward.turner@gmail.com>
2023-02-25 14:11:08 -0500

    Build js componenent file structure

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
 
 function App() {
   return (
